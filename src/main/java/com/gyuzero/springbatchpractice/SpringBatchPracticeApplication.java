8fb7093a45a70d202815f0031390a4f4ba42b1ff
gyuzero <gyuzero0103@gmail.com>
2025-02-06 14:22:42 +0900

    initial commit

---
+++ src/main/java/com/gyuzero/springbatchpractice/SpringBatchPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.gyuzero.springbatchpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchPracticeApplication.class, args);
+    }
+
+}
